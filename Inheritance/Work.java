077d5497838ef4d5edcd50e18840fb951fd3bb69
Amal Joe R S <amaljoe88@gmail.com>
2020-10-12 20:48:56 +0530

    super

---
+++ Inheritance/Work.java
@@ -0,0 +1,25 @@
+package Inheritance;
+
+class Employee{
+    void display(){
+        System.out.println("Name of the class is Employee");
+    }
+
+    void calcSalary(){
+        System.out.println("Salary of employee is 10000");
+    }
+}
+
+class Engineer extends Employee{
+    void calcSalary(){
+        super.calcSalary();
+        System.out.println("Salary of engineer is 20000");
+    }
+}
+class Work{
+    public static void main(String[] args) {
+        Engineer worker = new Engineer();
+        worker.display();
+        worker.calcSalary();
+    }
+}
--- TestGarbage.java
+++ TestGarbage.java
@@ -6,11 +6,11 @@ public class TestGarbage {
 
     public static void main(String args[]) {
         //creating objects for this class
-        TestGarbage s1 = new TestGarbage();
-        TestGarbage s2 = new TestGarbage();
+        // TestGarbage s1 = new TestGarbage();
+        // TestGarbage s2 = new TestGarbage();
         //pointing s1 and s2 to null so that it can be garbage collected
-        s1 = null;
-        s2 = null;
+        // s1 = null;
+        // s2 = null;
         //garbage collection
         System.gc();
     }
